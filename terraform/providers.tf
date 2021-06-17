bce27467287017cf22abb1e4ba480da89748fc79
Jose Hidalgo <joseangel1196@hotmail.com>
2021-06-16 23:14:40 -0400

    updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
   region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
